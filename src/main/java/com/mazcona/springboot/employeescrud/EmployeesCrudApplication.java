abc46fc94ff5e8d820ca3378c26371931fcebd91
marcelo-azcona <marceloazcona86@gmail.com>
2023-04-25 20:58:53 -0300

    Initial commit - uploading files

---
+++ src/main/java/com/mazcona/springboot/employeescrud/EmployeesCrudApplication.java
@@ -0,0 +1,12 @@
+package com.mazcona.springboot.employeescrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesCrudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeesCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mazcona/springboot/employeescrud/EmployeesCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mazcona.springboot.employeescrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
