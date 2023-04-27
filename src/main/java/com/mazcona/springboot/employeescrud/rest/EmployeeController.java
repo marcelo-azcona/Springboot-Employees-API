fcd20a58ecb021cb5748d0d1b85f21080a33b708
marcelo-azcona <marceloazcona86@gmail.com>
2023-04-27 20:43:43 -0300

    Added the mapping and CRUD methods to the controller

--- src/main/java/com/mazcona/springboot/employeescrud/rest/EmployeeController.java
+++ src/main/java/com/mazcona/springboot/employeescrud/rest/EmployeeController.java
@@ -3,9 +3,7 @@ package com.mazcona.springboot.employeescrud.rest;
 import com.mazcona.springboot.employeescrud.entity.Employee;
 import com.mazcona.springboot.employeescrud.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,53 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee findById(@PathVariable int employeeId) {
+        Employee employee = employeeService.findById(employeeId);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee not found");
+        }
+
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+
+        Employee dbEmployee;
+
+        // Auto Increment in the DB will generate the id
+        employee.setId(0);
+
+        dbEmployee = employeeService.save(employee);
+
+        return dbEmployee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+
+        Employee dbEmployee;
+
+        dbEmployee = employeeService.save(employee);
+
+        return dbEmployee;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteById(@PathVariable int employeeId) {
+
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        if (tempEmployee == null) {
+            throw new RuntimeException("Employee not found");
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee - " + employeeId + " " + tempEmployee.getLastName();
     }
 }
